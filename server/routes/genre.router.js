b0d1b4df95cf28ed136ead9111ddd1512b7d7acd
Paul Norling <paulnorling87@gmail.com>
2022-12-10 13:26:34 -0600

    set up genre.router

--- server/routes/genre.router.js
+++ server/routes/genre.router.js
@@ -4,7 +4,22 @@ const pool = require('../modules/pool')
 
 router.get('/', (req, res) => {
   // Add query to get all genres
-  res.sendStatus(500)
+  const query = `SELECT "genres"."name", count("genres"."name")
+  FROM "movies"
+  JOIN "movies_genres"
+  ON "movies"."id"="movies_genres"."movie_id"
+  JOIN "genres"
+  ON "movies_genres"."genre_id"="genres"."id"
+  GROUP BY "genres"."name";`
+  pool.query(query)
+    .then( result => {
+      res.send(result.rows);
+    })
+    .catch(err => {
+      console.log('ERROR: Get data', err);
+      res.sendStatus(500)
+    })
+    
 });
 
 module.exports = router;
