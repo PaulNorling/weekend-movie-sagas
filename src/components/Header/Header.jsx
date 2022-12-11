b86be8f757099778d4eff91f7f37960c922403b2
Paul Norling <paulnorling87@gmail.com>
2022-12-11 16:49:17 -0600

    added ringloader to header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,9 +1,14 @@
 import './Header.css'
+import { RingLoader } from 'react-spinners';
 
 function Header() {
     return (
         <header className="banner-header">
+            
+            <div className='header-container'>
+            <RingLoader className='ring-loader' color={'#00FFFF'} loading={true} />
             <h1>The Movies Saga!</h1>
+            </div>
         </header>
     );
 }
